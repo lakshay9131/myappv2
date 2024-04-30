7b43f9df4b73a92e83bd3fc2e2ae9b688a1372f4
akshay.moryani <akshay.moryani@eko.co.in>
2024-04-30 11:50:14 +0530

    modified 2

--- src/UmbrellaCustomizer.tsx
+++ src/UmbrellaCustomizer.tsx
@@ -157,15 +157,9 @@ const UmbrellaCustomizer: React.FC <Props>  = (Props) => {
               <span className="text verticalalign">{fileName}</span>
               {logoPreview && (
                 <button className="close-button" onClick={removeLogo}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-x"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M5.354 4.354a.5.5 0 0 1 .708 0L8 6.293l2.938-2.939a.5.5 0 1 1 .708.708L8.707 7l2.939 2.938a.5.5 0 1 1-.708.708L8 7.707l-2.938 2.939a.5.5 0 1 1-.708-.708L7.293 7 4.354 4.062a.5.5 0 0 1 0-.708z" />
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
                   </svg>
                 </button>
                 )}
